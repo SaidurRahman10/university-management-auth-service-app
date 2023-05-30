1a5515f1713d8dad4563167b12d80d587f14daf2
saidurrahman <saidurrahman1473@gmail.com>
2023-05-31 05:28:19 +0600

    [testing] husky

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
-
-
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
-    try {
-
-    await mongoose.connect(config.database_url as string);
+  try {
+    await mongoose.connect(config.database_url as string)
     console.log(`DB is Connected :)`)
-
     app.listen(config.port, () => {
-        console.log(`Sabbir Your Server is on ${config.port}`)
-      })
-  
-        // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-        
-    } catch (error) {
-        console.log("Faield to connect DB",error)
-    }
-   
+      console.log(`Sabbir Your Server is on ${config.port}`)
+    })
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  } catch (error) {
+    console.log('Faield to connect DB', error)
   }
-  bootstrap();
-  
+}
+bootstrap()
