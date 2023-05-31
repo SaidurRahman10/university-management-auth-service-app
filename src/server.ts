c46c36ac90ac7f52412a815b6126401fa2c98829
saidurrahman <saidurrahman1473@gmail.com>
2023-05-31 07:09:01 +0600

    [DONE] Create User Interface Model

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ import config from './config'
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`DB is Connected :)`)
+    console.log(`DB is Connected Successfully :)`)
     app.listen(config.port, () => {
-      console.log(`Sabbir Your Server is on ${config.port}`)
+      console.log(`Sabbir Your Server is on this port :  ${config.port}`)
     })
 
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
